26ed703f12b1162840731fee66a0bfd9fba62d28
FuriousNayan <npatel24@my.waketech.edu>
2026-01-12 19:57:55 -0500

    First exercise

---
+++ ExerciseOne.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+/**
+ * Exercise 1: Write 100 random integers to a text file
+ * 
+ * Requirements:
+ * - Create a file named Exercise17_01.txt if it does not exist
+ * - If the file already exists, APPEND new data (don't overwrite)
+ * - Write 100 integers created randomly (use Math.random())
+ * - Integers should be separated by a space
+ * - Use text I/O (PrintWriter)
+ * 
+ * @author Your Name
+ */
+public class ExerciseOne {
+    
+    public static void main(String[] args) throws IOException {
+        // TODO: Create a File object for "Exercise17_01.txt"
+        File file = new File("Exercise17_01.txt");
+        
+        
+        // TODO: Create PrintWriter in APPEND mode
+        // Hint: Use FileWriter with append parameter set to true
+        PrintWriter output = new PrintWriter(new FileWriter(file, true));
+        
+        
+        // TODO: Generate and write 100 random integers
+        // Hint: Use Math.random() to generate random integers
+        // Hint: Separate integers with a space
+        for(int i = 0; i < 100; i++){
+            int myNum = (int) (Math.random() * 101);
+            output.print(myNum + " ");
+        }
+        
+        
+        // TODO: Close the file (or use try-with-resources)
+        output.close();
+
+        
+        System.out.println("100 random integers written to Exercise17_01.txt");
+    }
+}
